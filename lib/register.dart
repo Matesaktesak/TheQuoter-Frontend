1905539fe16f5b831de3e27369c3c0c47136d1d9
Matyáš Levíček <46134697+Matesaktesak@users.noreply.github.com>
2022-06-08 22:30:12 +0200

    Implemented registration with class selector

--- lib/register.dart
+++ lib/register.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:thequoter_flutter_frontend/main.dart';
+import 'package:thequoter_flutter_frontend/models/class.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
   Map<String, String> appData;
-  final _registerFormKey = GlobalKey<FormState>();
-  
+  String? _classId;
+
   Register(this.appData, {Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final _registerFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +32,51 @@ class Register extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: "Username*",
-                  helperText: "Enter your desired user name",
-                ),
-                autofocus: true,
-                controller: _usernameController,
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        hintText: "Username*",
+                        helperText: "Enter your desired user name",
+                      ),
+                      autofocus: true,
+                      controller: _usernameController,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: FutureBuilder(
+                      future: api.getClasses(),
+                      builder: (context, AsyncSnapshot<List<Class>> snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          // If the API request has finnished
+                          return DropdownButton(
+                            hint: Text("Class"),
+                            value: widget._classId,
+                            items: snapshot.data?.map((Class c) {
+                              return DropdownMenuItem(
+                                value: c.id,
+                                child: Text(c.name),
+                              );
+                            }).toList(),
+                            onChanged: (String? value) { // On value changed
+                              if(value != null) { // If value is not null
+                                setState(() {
+                                  widget._classId = value;
+                                });
+                              }
+                            },
+                          );
+                        } else {
+                          // If the future is still loading
+                          return const CircularProgressIndicator();
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 12.0,
@@ -44,7 +89,9 @@ class Register extends StatelessWidget {
                 ),
                 controller: _emailController,
                 validator: (String? value) {
-                  return value!.contains("@") ? null : "Email address has to contain a '@'";
+                  return value!.contains("@")
+                      ? null
+                      : "Email address has to contain a '@'";
                 },
               ),
               const SizedBox(
@@ -77,7 +124,7 @@ class Register extends StatelessWidget {
               ElevatedButton(
                 onPressed: () {
                   //setState() {
-                    register(appData);
+                  if(widget._classId != null) register(widget.appData, Class(id: widget._classId!));
                   //}
                   //Navigator.pop(context);
                 },
@@ -90,21 +137,21 @@ class Register extends StatelessWidget {
     );
   }
 
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  TextEditingController _passwordController2 = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passwordController2 = TextEditingController();
 
-  void register(appData) async {
+  void register(appData, Class clas) async {
     print("Register clicked!");
     // TODO: Register functionality
-    validate();
+    if (!validate()) return;
 
     String username = _usernameController.text;
     String email = _emailController.text;
     String password = _passwordController.text;
 
-    String token = await api.register(username, email, password);
+    String token = await api.register(username, email, password, clas);
 
     appData["username"] = username;
     appData["jwt"] = token;
@@ -112,8 +159,8 @@ class Register extends StatelessWidget {
     print("username: $username, email: $email, password: $password");
   }
 
-  void validate() {
-    _registerFormKey.currentState!.validate();
+  bool validate() {
+    return _registerFormKey.currentState!.validate();
 
     // TODO: Fix validation
   }
