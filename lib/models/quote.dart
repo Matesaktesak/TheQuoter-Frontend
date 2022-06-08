cabe7e6d6f875372c2799ba3fcecd5df64ea3de7
Matyáš Levíček <46134697+Matesaktesak@users.noreply.github.com>
2022-06-08 22:28:55 +0200

    Added fromJson method, changed _id to id

--- lib/models/quote.dart
+++ lib/models/quote.dart
@@ -2,7 +2,7 @@ import 'package:thequoter_flutter_frontend/models/class.dart';
 import 'package:thequoter_flutter_frontend/models/person.dart';
 
 class Quote {
-  final String _id;
+  final String id;
   final String author;
   final String text;
   final String context;
@@ -10,5 +10,18 @@ class Quote {
   final Person originator;
   final Class clas;
 
-  const Quote(this._id, this.author, this.text, this.context, this.note, this.originator, this.clas);
+  const Quote(this.id, this.author, this.text, this.context, this.note, this.originator, this.clas);
+
+  // From a JSON object
+  static Quote fromJson(Map<String, dynamic> json) {
+    return Quote(
+      json["_id"],
+      json["author"],
+      json["text"],
+      json["context"],
+      json["note"],
+      Person.fromJson(json["originator"]),
+      Class.fromJson(json["class"]),
+    );
+  }
 }
