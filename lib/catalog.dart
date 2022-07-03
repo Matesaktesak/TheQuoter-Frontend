be234d4814926e1317a97399e101e598c0f31b96
Matyáš Levíček <matyas.levicek@levicek.net>
2022-07-03 23:00:56 +0200

    Added slidable actions to quotes

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
 import 'models/quote.dart';
@@ -30,9 +31,21 @@ class _CatalogState extends State<Catalog> {
         title: const Text("Catalog"),
         actions: [
           IconButton(
-            onPressed: () async {
-              final token = (await SharedPreferences.getInstance()).getString("token")!;
-              refresh(token);
+            onPressed: (){
+              beginSearch();
+            },
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: (){
+              openFilter();
+            },
+            icon: const Icon(Icons.filter_alt),
+          ),
+          IconButton(
+            onPressed: () {
+              final token = widget.settings.getString("token")!;
+              refresh(token, widget.settings.getString("role")!);
             },
             icon: const Icon(Icons.refresh),
           ),
@@ -44,7 +57,7 @@ class _CatalogState extends State<Catalog> {
           future: widget.futureQuotes,
           builder: (context, AsyncSnapshot<List<Quote>?> snapshot) {
             if(snapshot.connectionState == ConnectionState.none){
-              Future.microtask(() => refresh(widget.settings.getString("token")!));
+              Future.microtask(() => refresh(widget.settings.getString("token")!, widget.settings.getString("role")!));
             } else if(snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
             } else if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
@@ -61,33 +74,67 @@ class _CatalogState extends State<Catalog> {
                       topRight: Radius.circular(10.0),
                     )),
                     elevation: 3,
-                    child: ListTile(
-                      dense: true,
-                      tileColor: Colors.white,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10.0),
-                        topLeft: Radius.circular(10.0),
-                        topRight: Radius.circular(10.0),
-                      )),
-                      contentPadding: const EdgeInsets.fromLTRB(18.0, 6, 12.0, 12.0),
-                      title: Text(
-                        "„${snapshot.data![index].text}\"",
-                        style: _quoteTextTheme,
+                    child: Slidable(
+                      endActionPane: ActionPane(
+                        motion: const DrawerMotion(),
+                        children: [
+                          if(snapshot.data![index].state != Status.public && widget.settings.getString("role") == "admin") SlidableAction(
+                            onPressed: (context){
+                              api.setStatusQuote(
+                                token: widget.settings.getString("token")!,
+                                quote: snapshot.data![index],
+                                status: Status.public,
+                              ).then((e){
+                                 setState(() => refresh(widget.settings.getString("token")!, widget.settings.getString("role")!));  
+                              });
+                            },
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                            icon: Icons.check,
+                            label: "Approve",
+                          ),
+                          if(widget.settings.getString("role") == "admin")SlidableAction(
+                            onPressed: (context){
+                              api.deleteQuote(
+                                token: widget.settings.getString("token")!,
+                                quote: snapshot.data![index]
+                              );
+                            },
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            icon: Icons.delete,
+                            label: "Delete",
+                          ),
+                        ],
                       ),
-                      subtitle: Text(
-                        "- ${snapshot.data![index].originator.name}",
-                        style: Theme.of(context).textTheme.labelSmall,
-                        textAlign: TextAlign.right,
-                      ),
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => QuoteDisplay(settings: widget.settings, quote: snapshot.data![index])));
-                      },
-                      onLongPress: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => QuoteCreate(settings: widget.settings, isEdit: snapshot.data![index])));
-                      }
+                      child: ListTile(
+                        dense: true,
+                        tileColor: snapshot.data![index].state == Status.public ? Colors.white : const Color.fromARGB(255, 255, 169, 169),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0),
+                        )),
+                        contentPadding: const EdgeInsets.fromLTRB(18.0, 6, 12.0, 12.0),
+                        title: Text(
+                          "„${snapshot.data![index].text}\"",
+                          style: _quoteTextTheme,
+                        ),
+                        subtitle: Text(
+                          "- ${snapshot.data![index].originator.name}",
+                          style: Theme.of(context).textTheme.labelSmall,
+                          textAlign: TextAlign.right,
+                        ),
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => QuoteDisplay(settings: widget.settings, quote: snapshot.data![index])));
+                        },
+                        onLongPress: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => QuoteCreate(settings: widget.settings, isEdit: snapshot.data![index])));
+                        }
+                     ),
                     ),
-                  );
+                  ); 
                 },
               );
             }
@@ -99,9 +146,17 @@ class _CatalogState extends State<Catalog> {
     );
   }
 
-  void refresh(String token) {
+  void openFilter(){
+    // TODO: Implement
+  }
+
+  void beginSearch(){
+    // TODO: Implement
+  }
+
+  void refresh(String token, String role) {
     setState(() {
-      widget.futureQuotes =  api.getQuotesCatalog(token);
+      widget.futureQuotes =  api.getQuotesCatalog(token: token, role: role);
     });
   }
 
